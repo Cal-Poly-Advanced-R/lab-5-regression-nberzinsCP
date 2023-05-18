967e906931fcd5372d0c2837374335aa7fbfa549
Ben Laufer <nlaufer@calpoly.edu>
2023-05-17 23:05:08 -0700

    finished up ridge regression section

--- R/ridge_regression.R
+++ R/ridge_regression.R
@@ -16,18 +16,28 @@
 #' @import dplyr
 #'
 #' @export
+
 ridge_regression <- function(dat, response, lambda) {
 
+  # changes dataframe to matrix
+  dat_numeric <- as.matrix(dat)
 
+  #extracts the response/explanatory variables from our dataset
+  y <- dat_numeric[, response]
+  x <- dat_numeric[, !colnames(dat_numeric) %in% response]
 
-  results <- 0
-  ### This should be a data frame, with columns named
-  ### "Intercept" and the same variable names as dat, and also a column
-  ### called "lambda".
+  #coefficient matrix
+  lambda_diag <- diag(lambda, ncol(x))
+  #x_transpose <- t(x)
+  coef_matrix <- solve( t(x) %*% x + lambda_diag ) %*% t(x) %*% y
+
+  #creates our dataframe
+  results <- data.frame("Intercept" = coef_matrix[1], as.list(coef_matrix[-1]), "lambda" = lambda)
 
   return(results)
-
 }
+
+
 
 #' Determines the best penalty term from a set of options
 #'
@@ -39,7 +49,7 @@ ridge_regression <- function(dat, response, lambda) {
 #' @param train_dat A data frame to construct the model from
 #' @param test_dat A data frame to test the model on
 #' @param response The name of a response variable in the data frame (unquoted)
-#' @param lambda A vector of penalty terms to try
+#' @param lambdas A vector of penalty terms to try
 #'
 #' @return A data frame of penalty terms and resulting errors
 #'
@@ -48,11 +58,31 @@ ridge_regression <- function(dat, response, lambda) {
 #' @export
 find_best_lambda <- function(train_dat, test_dat, response, lambdas) {
 
+  # creates dataframe
+  lambda_errors <- data.frame(lambda = numeric(),
+                              error = numeric(),
+                              stringsAsFactors = FALSE)
 
-  ### lambda_errors should be a data frame with two columns: "lambda" and "error"
-  ### For each lambda, you should record the resulting Sum of Squared error
-  ### (i.e., the predicted value minus the real value squared) from prediction
-  ### on the test dataset.
+  for (lambda in lambdas) {
+    # coefficients called ridge_regression function
+    coefficients <- ridge_regression(train_dat, response, lambda)
+
+    # extracts the response/explanatory variables from our dataset
+    y <- test_dat[, response]
+    x <- test_dat[, !colnames(test_dat) %in% response]
+
+    # predicted values
+    y_predicted <- as.matrix(x) %*% t(as.matrix(coefficients[-1])) + coefficients[1]
+
+    # sum of squared errors
+    squared_error <- sum((y_predicted - y)^2)
+
+    # binds variable to our dataframe
+    lambda_errors <- rbind(lambda_errors, data.frame(lambda = lambda,
+                                                     squared_error = squared_error))
+  }
 
   return(lambda_errors)
 }
+
+
